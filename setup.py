72b44188f3608e6f74b911598f7307d4848fbe87
Sean Jones <sean.d.jones92@gmail.com>
2019-11-18 08:15:24 -0700

    add pipenv to setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'Click'
+        'Click',
+        'pipenv'
     ],
     entry_points='''
         [console_scripts]
