b914625e62cd20be4108040f45d07d13e1d137b0
Sean Jones <sean.d.jones92@gmail.com>
2020-06-27 08:26:39 -0600

    move version logic to new file

---
+++ pyproject/versionhandler.py
@@ -0,0 +1,29 @@
+>#!/usr/bin/env python
+
+class versionhandler(object):
+    
+    def __init__(self, number):
+        # setup regex patterns and version strings for files to change
+        self.setuplocation = 'setup.py'
+        self.setupversion = 'version="' + number + '"'
+        self.setuppattern = 'version=".*"'
+
+        self.snaplocation = 'snap/snapcraft.yaml'
+        self.snapversion = 'version: \'' + number + '\''
+        self.snappattern = 'version: \'.*\''
+
+        # build a list of the above information to make iteration easier
+        self.versionlisting = []
+        self.versionlisting.append([setuplocation, setupversion, setuppattern])
+        self.versionlisting.append([snaplocation, snapversion, snappattern])
+
+    def update(self):
+        # iterate through the above list and apply the version change
+        for i in versionlisting:
+            workingfile = open(i[0], 'rt')
+            filedata = workingfile.read()
+            workingfile.close()
+            filedata = re.sub(i[2], i[1], filedata)
+            workingfile = open(i[0], 'wt')
+            workingfile.write(filedata)
+            workingfile.close()
