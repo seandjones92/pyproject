f4afbbd0ecdc0e24f8059669a8044f4ddfa6be9d
Sean Jones <sean.d.jones92@gmail.com>
2020-06-27 08:30:24 -0600

    init file calls new version logic

--- pyproject/__init__.py
+++ pyproject/__init__.py
@@ -4,6 +4,7 @@ import click
 import re
 
 from pyproject import templatebuilder
+from versionhandler import versionhandler
 
 
 @click.group()
@@ -55,35 +56,37 @@ def gui(name):
 @click.command()
 @click.argument('number')
 def version(number):
-    """Set the version of the current project.
+    vhobject = versionhandler(number)
+    vhobject.update()
+#     """Set the version of the current project.
 
-    Pass a number to indicate the new version of the project.
-    For example: 1.2
-    """
+#     Pass a number to indicate the new version of the project.
+#     For example: 1.2
+#     """
 
-    # setup regex patterns and version strings for files to change
-    setuplocation = 'setup.py'
-    setupversion = 'version="' + number + '"'
-    setuppattern = 'version=".*"'
+#     # setup regex patterns and version strings for files to change
+#     setuplocation = 'setup.py'
+#     setupversion = 'version="' + number + '"'
+#     setuppattern = 'version=".*"'
 
-    snaplocation = 'snap/snapcraft.yaml'
-    snapversion = 'version: \'' + number + '\''
-    snappattern = 'version: \'.*\''
+#     snaplocation = 'snap/snapcraft.yaml'
+#     snapversion = 'version: \'' + number + '\''
+#     snappattern = 'version: \'.*\''
 
-    # build a list of the above information to make iteration easier
-    versionlisting = []
-    versionlisting.append([setuplocation, setupversion, setuppattern])
-    versionlisting.append([snaplocation, snapversion, snappattern])
+#     # build a list of the above information to make iteration easier
+#     versionlisting = []
+#     versionlisting.append([setuplocation, setupversion, setuppattern])
+#     versionlisting.append([snaplocation, snapversion, snappattern])
 
-    # iterate through the above list and apply the version change
-    for i in versionlisting:
-        workingfile = open(i[0], 'rt')
-        filedata = workingfile.read()
-        workingfile.close()
-        filedata = re.sub(i[2], i[1], filedata)
-        workingfile = open(i[0], 'wt')
-        workingfile.write(filedata)
-        workingfile.close()
+#     # iterate through the above list and apply the version change
+#     for i in versionlisting:
+#         workingfile = open(i[0], 'rt')
+#         filedata = workingfile.read()
+#         workingfile.close()
+#         filedata = re.sub(i[2], i[1], filedata)
+#         workingfile = open(i[0], 'wt')
+#         workingfile.write(filedata)
+#         workingfile.close()
 
 
 entrypoint.add_command(create)
